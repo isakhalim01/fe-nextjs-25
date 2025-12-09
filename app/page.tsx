db6ace48d572c8801fea68052bd184f8284da372
isakhalim01 <khalimzamil19@gmail.com>
2025-12-08 22:27:11 -0500

    Initialize Next.js project with TypeScript, ESLint, and Tailwind CSS. Add configuration files, global styles, and basic layout components. Include README and .gitignore for project setup.

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import Button from "@/components/ui/Button";
+import Layout from "@/components/ui/Layout";
+
+export default function Home() {
+  return (
+  <Layout>
+    <h1>Home</h1>
+    <Button />
+  </Layout>
+  );
+}
